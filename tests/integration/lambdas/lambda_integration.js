e1d13c2976b11ef36a1be60a6f0e196f4b516fd9
Fabiano Oliveira <eu@fabiano.xyz>
2018-04-24 11:11:58 -0300

    Cleaning up sources of the localstack

--- tests/integration/lambdas/lambda_integration.js
+++
@@ -1,9 +0,0 @@
-exports.handler = function(event, context, callback) {
-	console.log('Node.js Lambda handler executing.');
-	var result = {};
-	if(callback) {
-		callback(result);
-	} else {
-		context.succeed(result);
-	}
-};
--- tests/integration/test_web_ui.py
+++
@@ -1,9 +0,0 @@
-from localstack.dashboard import infra
-
-
-def test_infra_graph_generation():
-    graph = infra.get_graph()
-    assert 'nodes' in graph
-    assert 'edges' in graph
-
-    # TODO add more tests/assertions
